ea881601663c310482bf7db5fa636f58a741f84b
AlissonMunoz78 <alisson.munoz@epn.edu.ec>
2024-07-29 20:17:52 -0500

    Add files via upload

---
+++ src/src2/PersonalMedico2.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class PersonalMedico2 {
+    private JButton registrarCitaButton;
+    private JButton historialMédicoButton;
+    private JButton resultadoExamenesButton;
+    private JButton tratamientoButton;
+    private JButton salirButton;
+}
